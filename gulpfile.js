4141bcd49f32d6c8d2dd1c7b3046b1d576f999c0
Trevor Fountain <trevor@texasexpat.net>
2015-10-20 16:45:27 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var less = require('gulp-less');
+var rename = require("gulp-rename");
+var es = require('event-stream');
+var runSequence = require('run-sequence');
+var wiredep = require('wiredep').stream;
+var fileinclude = require('gulp-file-include');
+
+// Compile less into CSS
+gulp.task('less', function () {
+  return gulp.src(['./src/**/*.less'])
+    .pipe(less({
+      paths: [ './src/less/*.less' ]
+    }))
+    .pipe(rename({dirname: "css"}))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task("images", function () {
+  return gulp.src(["./src/img/*"])
+  .pipe(rename({dirname: "assets"}))
+  .pipe(gulp.dest('./dist/'))
+  .pipe(reload({stream: true}));
+})
+
+gulp.task("fonts", function () {
+  return gulp.src(["./src/fonts/*/*.woff", "./src/fonts/*/*.woff2", "./src/fonts/*/*.ttf", "./src/fonts/*/*.eot", "./src/fonts/*/*.svg"])
+  .pipe(rename({dirname: "css/fonts/"}))
+  .pipe(gulp.dest('./dist/'))
+  .pipe(reload({stream: true}));
+})
+
+gulp.task('dev', ['build', 'serve-dev']);
+
+gulp.task("dist", function(callback) {
+  runSequence("build", callback);
+});
+
+gulp.task("build", function(callback) {
+  runSequence(["less", "images", "fonts"], callback);
+});
+
+// Watch all source files for changes
+gulp.task('serve-dev', function() {
+  gulp.watch(['src/**/*.less'], ['less']);
+  gulp.watch(['src/**/*'], ["images"]);
+
+    // Reload browser-sync on change
+  browserSync.init({
+    server: {
+      baseDir: ["./dist", "./bower_components", "./examples", "./dist/assets"]
+    }
+  });
+
+  gulp.watch(['./dist/**/*', "examples/**/*"]).on('change', function(e) {
+    console.log(e.type + ": " + e.path);
+    reload();
+  });
+});
